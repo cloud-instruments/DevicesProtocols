c7b4b0984495aff216c3be28518486cbc8c60575
Matteo Lucarelli <matteo.lucarelli@toptal.com>
2017-05-19 11:53:05 +0200

    MacxcorU1 v0.1: UserDLL for Maccor, no functionality, sketch only

---
+++ MaccorU1/MaccorU1Test1/MaccorU1Test1.cpp
@@ -0,0 +1,17 @@
+// MaccorU1Test1.cpp : Defines the entry point for the console application.
+//
+
+#pragma comment(lib, "MaccorU1.lib")
+
+#include "stdafx.h"
+#include <windows.h>
+#include "../MaccorU1/MaccorU1.h"
+
+int main()
+{
+
+
+
+    return 0;
+}
+
